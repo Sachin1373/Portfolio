705d5901001828d52c38bfa43a1409e582d9cfc7
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-09-24 22:51:48 +0530

    updated

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -11,98 +11,98 @@ const SkillS = () => {
          <ul className="Collection">
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-java ci-6x"></i>
+                    <i className="ci ci-java ci-5x"></i>
                     <div className="skill-name">JAVA</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-html ci-6x"></i>
+                    <i className="ci ci-html ci-5x"></i>
                     <div className="skill-name">HTML</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-css ci-6x"></i>
+                    <i className="ci ci-css ci-5x"></i>
                     <div className="skill-name">CSS</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-js ci-6x"></i>
+                    <i className="ci ci-js ci-5x"></i>
                     <div className="skill-name">JS</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-react ci-6x"></i>
+                    <i className="ci ci-react ci-5x"></i>
                     <div className="skill-name">React Js</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-node ci-6x"></i>
+                    <i className="ci ci-node ci-5x"></i>
                     <div className="skill-name">Node Js</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-mongodb ci-6x"></i>
+                    <i className="ci ci-mongodb ci-5x"></i>
                     <div className="skill-name">Mongo DB</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-expressjs ci-6x"></i>
+                    <i className="ci ci-expressjs ci-5x"></i>
                     <div className="skill-name">Express Js</div>
                 </div>
             </li>
             <li>
                 
                 <div className="skill-item">
-                    <i className="ci ci-mysql ci-6x"></i>
+                    <i className="ci ci-mysql ci-5x"></i>
                     <div className="skill-name">SQL</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-postman ci-6x"></i>
+                    <i className="ci ci-postman ci-5x"></i>
                     <div className="skill-name">Postman</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-git ci-6x"></i>
+                    <i className="ci ci-git ci-5x"></i>
                     <div className="skill-name">Git</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-python ci-6x"></i>
+                    <i className="ci ci-python ci-5x"></i>
                     <div className="skill-name">Python</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-azure ci-6x"></i>
+                    <i className="ci ci-azure ci-5x"></i>
                     <div className="skill-name">AZURE</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-npm ci-6x"></i>
+                    <i className="ci ci-npm ci-5x"></i>
                     <div className="skill-name">npm</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-tailwind ci-6x"></i>
+                    <i className="ci ci-tailwind ci-5x"></i>
                     <div className="skill-name">Tailwind CSS</div>
                 </div>
             </li>
             <li>
                 <div className="skill-item">
-                    <i className="ci ci-redux ci-6x"></i>
+                    <i className="ci ci-redux ci-5x"></i>
                     <div className="skill-name">Redux</div>
                 </div>
             </li>
