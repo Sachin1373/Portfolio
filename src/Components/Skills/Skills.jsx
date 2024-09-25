9927fda8fcbfbca9188b75efedd0a0cf8fe5316e
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-09-25 14:13:59 +0530

    updtaed

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,116 +1,123 @@
-
-import React from 'react';
-import './Skills.css'; 
-
+import React, { useState, useEffect } from 'react';
+import './Skills.css';
 
 const SkillS = () => {
-  return (
-   <>
-   <div className="skillcon">
-         <h1>Skills</h1>
-         <ul className="Collection">
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-java ci-5x"></i>
-                    <div className="skill-name">JAVA</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-html ci-5x"></i>
-                    <div className="skill-name">HTML</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-css ci-5x"></i>
-                    <div className="skill-name">CSS</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-js ci-5x"></i>
-                    <div className="skill-name">JS</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-react ci-5x"></i>
-                    <div className="skill-name">React Js</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-node ci-5x"></i>
-                    <div className="skill-name">Node Js</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-mongodb ci-5x"></i>
-                    <div className="skill-name">Mongo DB</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-expressjs ci-5x"></i>
-                    <div className="skill-name">Express Js</div>
-                </div>
-            </li>
-            <li>
-                
-                <div className="skill-item">
-                    <i className="ci ci-mysql ci-5x"></i>
-                    <div className="skill-name">SQL</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-postman ci-5x"></i>
-                    <div className="skill-name">Postman</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-git ci-5x"></i>
-                    <div className="skill-name">Git</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-python ci-5x"></i>
-                    <div className="skill-name">Python</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-azure ci-5x"></i>
-                    <div className="skill-name">AZURE</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-npm ci-5x"></i>
-                    <div className="skill-name">npm</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-tailwind ci-5x"></i>
-                    <div className="skill-name">Tailwind CSS</div>
-                </div>
-            </li>
-            <li>
-                <div className="skill-item">
-                    <i className="ci ci-redux ci-5x"></i>
-                    <div className="skill-name">Redux</div>
-                </div>
-            </li>
-        
-         </ul>
+  const [isMobile, setIsMobile] = useState(false);
 
-   </div>
-   </>
+  // Check if the screen size is mobile on initial render and resize
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 480);
+    };
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
+  return (
+    <>
+      <div className="skillcon">
+        <h1>Skills</h1>
+        <ul className="Collection">
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-java ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">JAVA</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-html ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">HTML</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-css ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">CSS</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-js ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">JavaScript</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-react ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">React Js</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-node ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Node Js</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-mongodb ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">MongoDB</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-expressjs ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Express Js</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-mysql ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">SQL</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-postman ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Postman</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-git ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Git</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-python ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Python</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-azure ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Azure</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-npm ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">npm</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-tailwind ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Tailwind CSS</div>
+            </div>
+          </li>
+          <li>
+            <div className="skill-item">
+              <i className={`ci ci-redux ${isMobile ? 'ci-4x' : 'ci-6x'}`}></i>
+              <div className="skill-name">Redux</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
